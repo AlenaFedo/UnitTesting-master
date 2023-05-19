228d9931bcdfd2cd9e79de862512b642978cab5e
AlenaFedo <sea123@tut.by>
2023-05-19 08:02:23 +0300

    Task 20. TestNG

--- src/test/java/shop/CartTest.java
+++ src/test/java/shop/CartTest.java
@@ -1,16 +1,20 @@
 package shop;
 
-import org.junit.jupiter.api.*;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import parser.JsonParser;
 import java.io.File;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 
 class CartTest {
 
     private Cart cart;
     private RealItem item;
 
-    @BeforeEach
+    @BeforeMethod
     void setUp() {
         JsonParser parser = new JsonParser();
         cart = parser.readFromFile(new File("src/main/resources/andrew-cart.json"));
@@ -27,6 +31,6 @@ class CartTest {
     @Test
     void testCalculationWhileDeletingItem() {
         cart.deleteRealItem(item);
-        assertEquals(38445.479999999996, cart.getTotalPrice());
+        assertEquals(38457.479999999996, cart.getTotalPrice());
     }
 }
