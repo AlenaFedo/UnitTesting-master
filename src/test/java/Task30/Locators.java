5b797d98c56b55c833f2d4f0f6dfc6c72ce0c68e
AlenaFedo <sea123@tut.by>
2023-06-05 15:21:21 +0300

    post review

--- src/test/java/Task30/Locators.java
+++ src/test/java/Task30/Locators.java
@@ -1,39 +1,14 @@
 package Task30;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Locators {
 
-    @Test
-    public void task() {
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\SeleniumTasks\\SeleniumJavaTraining\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public static final By LOGIN_BUTTON = By.xpath("//a[contains(@class,'Button2_view_default Button2_size_m')]");
+    public static final By USERNAME_FIELD = By.id("passp-field-login");
+    public static final By PASSWORD_FIELD = By.xpath("//input[@id ='passp-field-passwd']");
+    public static final By SUBMIT_BUTTON = By.xpath("//button[contains(@class,'submit')]");
+    public static final By INBOX = By.xpath("//a[contains(@aria-label, 'Inbox')]");
 
-        driver.get("https://mail.yandex.com/");
-
-        //Button Login
-        Assertions.assertTrue(driver.findElement(By.xpath("//a[contains(@class,'Button2_view_default Button2_size_m')]")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.cssSelector(".Button2_type_link.Button2_view_default")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.linkText("Log in")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.cssSelector("[href='https://passport.yandex.com/auth?retpath=https%3A%2F%2Fmail.yandex.com']")).isDisplayed());
-
-        //Buy
-        Assertions.assertTrue(driver.findElement(By.cssSelector(".Button2_size_m.ActionButton_VXdGh2PasdGWTKaK2Xlzy")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.xpath("//a[contains(@class,'Button2_view_action Button2_size_m')]")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.linkText("Buy")).isDisplayed());
-
-        Assertions.assertTrue(driver.findElement(By.xpath("//span[@class = 'Button2-Text' and text()='Products' ]")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.xpath("//span[@class = 'Button2-Text' and text()='Solutions' ]")).isDisplayed());
-        Assertions.assertTrue(driver.findElement(By.xpath("//span[@class = 'Button2-Text' and text()='Plans' ]")).isDisplayed());
-
-        Assertions.assertTrue(driver.findElement(By.linkText("Get started")).isDisplayed());
-
-        driver.quit();
-    }
 }
